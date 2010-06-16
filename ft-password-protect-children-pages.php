7609803392a329de91ec2f728b8143a6f0a29ae7
blepoxp <blepoxp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-16 15:21:59 +0000

    Adding ability to protect grandchildren pages
    git-svn-id: https://plugins.svn.wordpress.org/ft-password-protect-children-pages/tags/0.1@253127 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ft-password-protect-children-pages.php
+++ ft-password-protect-children-pages.php
@@ -2,14 +2,14 @@
 /**
  * @package FT_Password_Protect_Children_Pages
  * @author FullThrottleDevelopment.com
- * @version 0.2
+ * @version 1.0
  */
 /*
 Plugin Name: FT Password Protect Children Pages
 Plugin URI: http://fullthrottledevelopment.com/password-protect-children-pages
 Description: This plugin does one thing. If a page that is password protected has children pages, all children pages will be protected with the same password. If the correct password is entered on the parent page or any of its children pages, all related pages will be viewable to the user.
 Author: FullThrottle Development
-Version: 0.2
+Version: 0.1
 Author URI: http://fullthrottledevelopment.com/
 Primary Developer: Glenn Ansley (glenn@glennansley.com)
 */
@@ -18,13 +18,10 @@ Primary Developer: Glenn Ansley (glenn@glennansley.com)
 function ft_password_protect_children_page_contents( $org_content ){
 	if ( is_page() ){
 		global $post;
-
-		$ancestor = end( $post->ancestors );
-		
-		if ( !empty( $ancestor) ){
-			if ( post_password_required( $ancestor ) ) {
+		if ( !empty($post->post_parent) ){
+			if ( post_password_required( $post->post_parent ) ) {
 				$real_post = $post;
-				$post = $ancestor;
+				$post = $real_post->post_parent;
 			
 				echo get_the_password_form();
 				$post = $real_post;
@@ -40,11 +37,8 @@ add_filter( 'the_content', 'ft_password_protect_children_page_contents' );
 function ft_password_protect_children_page_excerpts( $org_excerpt ){
 	if ( is_page() ){
 		global $post;
-		
-		$ancestor = end( $post->ancestors );
-
-		if ( !empty( $ancestor ) ){
-			if ( post_password_required( $ancestor ) ) {
+		if ( !empty($post->post_parent) ){
+			if ( post_password_required( $post->post_parent ) ) {
 				$output = wpautop( __('There is no excerpt because this is a protected post.') );
 				return $output;
 			}
@@ -55,24 +49,23 @@ function ft_password_protect_children_page_excerpts( $org_excerpt ){
 add_filter( 'get_the_excerpt', 'ft_password_protect_children_page_excerpts' , 9);
 
 // This function alter's the Post Title to include the protected_title_format
-function ft_password_protect_children_page_titles( $org_title, $title_id='' ){
-	if ( is_page() && in_the_loop() ){
-	
+function ft_password_protect_children_page_titles( $org_title , $title_id='' ){
+	if ( is_page() ){
 		global $post;
-		
-		$ancestor = end( $post->ancestors );
-		$ancestor_post = get_post( $ancestor );
-		
-		if ( !empty( $ancestor ) ){
-			if ( post_password_required( $ancestor ) || isset( $ancestor_post->post_password ) ) {
-
-				$protected_title_format = apply_filters( 'protected_title_format', __('Protected: %s') );
-				$title = sprintf( $protected_title_format, $org_title );
-				return $title;
+		if ( !empty($post->post_parent) ){
+			if ( post_password_required( $post->post_parent ) ) {
+				$real_post = $post;
+				$post = $real_post->post_parent;
+				if( $real_post->ID === $title_id ){ 
+					$protected_title_format = apply_filters('protected_title_format', __('Protected: %s'));
+					$title = sprintf($protected_title_format, $org_title);
+					$post = $real_post;
+					return $title;
+				}
 			}
 		}
 	}
 	return $org_title;
 }
-add_filter( 'the_title', 'ft_password_protect_children_page_titles', 10 , 2 );
+add_filter( 'the_title' , 'ft_password_protect_children_page_titles', 10 , 2 );
 ?>
