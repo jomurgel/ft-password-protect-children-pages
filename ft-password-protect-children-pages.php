37355574f2bb9c6314a13e51fd0f60a54e496089
jomurgel <dev@jomurgel.com>
2016-12-14 18:20:11 -0800

    updated plugin header

--- ft-password-protect-children-pages.php
+++ ft-password-protect-children-pages.php
@@ -1,18 +1,24 @@
 <?php
 /**
- * @package FT_Password_Protect_Children_Pages
- * @author FullThrottleDevelopment.com
- * @version 0.3
+ * Plugin Name
+ *
+ * @package     Password_Protect_Children_Pages
+ * @author      jomurgel, fullthrottledevelopment, blepoxp
+ * @copyright   2010 FullThrottleDevelopment.com
+ * @license     GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name: Password Protect Children Pages
+ * Plugin URI:  https://github.com/jomurgel/ft-password-protect-children-pages
+ * Description: Password protect children of parent if parent is password protected.
+ * Version:     0.9
+ * Author: jomurgel
+ * Author URI: http://jomurgel.com
+ * Primary Developer: Glenn Ansley (glenn@glennansley.com)
+ * Text Domain: password-protect-children-pages
+ * License:     GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
-/*
-Plugin Name: FT Password Protect Children Pages
-Plugin URI: http://fullthrottledevelopment.com/password-protect-children-pages
-Description: This plugin does one thing. If a page that is password protected has children pages, all children pages will be protected with the same password. If the correct password is entered on the parent page or any of its children pages, all related pages will be viewable to the user.
-Author: FullThrottle Development
-Version: 0.3
-Author URI: http://fullthrottledevelopment.com/
-Primary Developer: Glenn Ansley (glenn@glennansley.com)
-*/
 
 // This function prints the password form if the parent page is password protected. It is called whenever 'the_content' is invoked.
 function ft_password_protect_children_page_contents( $org_content ){
@@ -21,14 +27,14 @@ function ft_password_protect_children_page_contents( $org_content ){
 
 		// Grab ancestors
 		$ancestors = $post->ancestors;
-		
+
 		// Loop through ancestors, grab first one that is password protected
 		foreach ( $ancestors as $ancestor ) {
-		
+
 			if ( post_password_required( $ancestor ) ) {
 				$real_post = $post;
 				$post = get_post( $ancestor );
-			
+
 				echo get_the_password_form();
 				$post = $real_post;
 				return;
@@ -43,7 +49,7 @@ add_filter( 'the_content', 'ft_password_protect_children_page_contents' );
 function ft_password_protect_children_page_excerpts( $org_excerpt ){
 	if ( is_page() ){
 		global $post;
-		
+
 		// Grab ancestors
 		$ancestors = $post->ancestors;
 
@@ -64,15 +70,15 @@ function ft_password_protect_children_page_titles( $org_title, $title_id='' ){
 	if ( is_page() && in_the_loop() ){
 
 		global $post;
-		
+
 		// Grab ancestors
 		$ancestors = $post->ancestors;
 
 		// Loop through ancestors, grab first one that is password protected
 		foreach ( $ancestors as $ancestor ) {
-			
+
 			$ancestor_post = get_post( $ancestor );
-			
+
 			if ( post_password_required( $ancestor ) || ( isset( $ancestor_post->post_password ) && !empty( $ancestor_post->post_password ) ) ) {
 
 				$protected_title_format = apply_filters( 'protected_title_format', __('Protected: %s') );
